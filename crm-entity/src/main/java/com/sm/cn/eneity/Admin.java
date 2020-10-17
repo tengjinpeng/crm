836262d63c3e0bc17aa45debac936113ef4ddd0f
tengjinpeng <524836410@qq.com>
2020-10-17 16:31:26 +0800

    集成测试

---
+++ crm-entity/src/main/java/com/sm/cn/eneity/Admin.java
@@ -0,0 +1,17 @@
+package com.sm.cn.eneity;
+
+import lombok.Data;
+
+/**
+ * @Auther: tjp
+ * @Date: 2020/10/16 22:17
+ * @Description:
+ */
+@Data
+public class Admin {
+private  Integer  adminId;
+private String adminName;
+private String adminPhone;
+
+
+}
