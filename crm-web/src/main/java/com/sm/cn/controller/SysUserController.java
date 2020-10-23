97b22aea6c2507c1a5685983c0923735f65552d1
tengjinpeng <524836410@qq.com>
2020-10-23 21:25:20 +0800

    动态加密

--- crm-web/src/main/java/com/sm/cn/controller/SysUserController.java
+++ crm-web/src/main/java/com/sm/cn/controller/SysUserController.java
@@ -5,7 +5,7 @@ package com.sm.cn.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.sm.cn.email.EmailService;
+
 import com.sm.cn.entity.SysUser;
 import com.sm.cn.groupvalidator.AddGroup;
 import com.sm.cn.groupvalidator.UpdateGroup;
@@ -13,17 +13,15 @@ import com.sm.cn.http.AjaxResult;
 import com.sm.cn.http.PageResult;
 
 import com.sm.cn.service.ISysUserService;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
 
 import javax.annotation.Resource;
-import javax.mail.MessagingException;
-import java.io.IOException;
+
+
 import java.io.Serializable;
 import java.io.StringWriter;
 import java.util.HashMap;
@@ -45,11 +43,7 @@ public class SysUserController {
 @Resource
 ISysUserService iSysUserService;
 @Autowired
-    BCryptPasswordEncoder bCryptPasswordEncoder;
-@Autowired
-private EmailService emailTest;
-@Autowired
-private FreeMarkerConfigurer freeMarkerConfigurer;
+private BCryptPasswordEncoder bCryptPasswordEncoder;
     @GetMapping
     public AjaxResult findAll(){
         List<SysUser> all = iSysUserService.findAll();
@@ -65,19 +59,9 @@ private FreeMarkerConfigurer freeMarkerConfigurer;
     }
 
     @PostMapping
-    public  AjaxResult add(@Validated(AddGroup.class) @RequestBody SysUser sysUser ) throws IOException, TemplateException, MessagingException {
-//        设置密码并动态加密到数据库，发邮箱验证
-         sysUser.setPassword(bCryptPasswordEncoder.encode("123456"));
-        freemarker.template.Configuration configuration=freeMarkerConfigurer.getConfiguration();
-        Template template=configuration.getTemplate("active.ftl","utf-8");
-        Map<String,String> map=new HashMap<>();
-        map.put("userName",sysUser.getUserName());
-        map.put("phone",sysUser.getPhone());
-        map.put("email",sysUser.getEmail());
-        map.put("password","123456");
-        StringWriter stringWriter=new StringWriter();
-        template.process(map,stringWriter);
-        emailTest.sendMail(sysUser.getEmail(),stringWriter.toString());
+    public  AjaxResult add(@Validated(AddGroup.class) @RequestBody SysUser sysUser ) throws Exception {
+
+      sysUser.setPassword(bCryptPasswordEncoder.encode("123456"));
          iSysUserService.add(sysUser);
         return AjaxResult.success();
     }
